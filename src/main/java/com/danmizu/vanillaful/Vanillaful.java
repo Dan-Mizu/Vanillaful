e19cd1228fde1c5c5b0ba15baad968bb4af9ee83
Danny <baghdadi.danny@gmail.com>
2020-07-01 20:35:54 -0700

    fuel fix

--- src/main/java/com/danmizu/vanillaful/Vanillaful.java
+++ src/main/java/com/danmizu/vanillaful/Vanillaful.java
@@ -4,6 +4,8 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.metadata.ModMetadata;
 
@@ -11,10 +13,12 @@ import net.minecraft.client.render.RenderLayer;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.BlockSoundGroup;
-
+import net.minecraft.tag.Tag;
+import net.minecraft.block.Block;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -25,6 +29,14 @@ public class Vanillaful implements ModInitializer {
     public static final ItemGroup VANILLAFUL = FabricItemGroupBuilder.build(
 		new Identifier("vanillaful", "vanillaful"),
 		() -> new ItemStack(Vanillaful.IRON_LADDER));
+
+	//Tags
+	//Ladders
+	public static final Tag<Block> LADDERS = TagRegistry.block(new Identifier("vanillaful", "ladders"));
+	//Ladders Fuel
+	public static final Tag<Item> LADDERS_FUEL = TagRegistry.item(new Identifier("vanillaful", "flammable_ladders"));
+	//Sticks Fuel
+	public static final Tag<Item> STICKS_FUEL = TagRegistry.item(new Identifier("vanillaful", "flammable_sticks"));
 
 	//Add Blocks/Items
 	//Iron Bar
@@ -75,54 +87,51 @@ public class Vanillaful implements ModInitializer {
 		//Iron Ladder
 		Registry.register(Registry.BLOCK, new Identifier("vanillaful", "iron_ladder"), IRON_LADDER);
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "iron_ladder"), new BlockItem(IRON_LADDER, new Item.Settings().group(Vanillaful.VANILLAFUL)));
-		BlockRenderLayerMap.INSTANCE.putBlock(Vanillaful.IRON_LADDER, RenderLayer.getCutout());
 		//Spruce Stick
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "spruce_stick"), SPRUCE_STICK);
 		//Spruce Ladder
 		Registry.register(Registry.BLOCK, new Identifier("vanillaful", "spruce_ladder"), SPRUCE_LADDER);
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "spruce_ladder"), new BlockItem(SPRUCE_LADDER, new Item.Settings().group(Vanillaful.VANILLAFUL)));
-		BlockRenderLayerMap.INSTANCE.putBlock(Vanillaful.SPRUCE_LADDER, RenderLayer.getCutout());
 		//Birch Stick
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "birch_stick"), BIRCH_STICK);
 		//Birch Ladder
 		Registry.register(Registry.BLOCK, new Identifier("vanillaful", "birch_ladder"), BIRCH_LADDER);
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "birch_ladder"), new BlockItem(BIRCH_LADDER, new Item.Settings().group(Vanillaful.VANILLAFUL)));
-		BlockRenderLayerMap.INSTANCE.putBlock(Vanillaful.BIRCH_LADDER, RenderLayer.getCutout());
 		//Jungle Stick
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "jungle_stick"), JUNGLE_STICK);
 		//Jungle Ladder
 		Registry.register(Registry.BLOCK, new Identifier("vanillaful", "jungle_ladder"), JUNGLE_LADDER);
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "jungle_ladder"), new BlockItem(JUNGLE_LADDER, new Item.Settings().group(Vanillaful.VANILLAFUL)));
-		BlockRenderLayerMap.INSTANCE.putBlock(Vanillaful.JUNGLE_LADDER, RenderLayer.getCutout());
 		//Acacia Stick
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "acacia_stick"), ACACIA_STICK);
 		//Acacia Ladder
 		Registry.register(Registry.BLOCK, new Identifier("vanillaful", "acacia_ladder"), ACACIA_LADDER);
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "acacia_ladder"), new BlockItem(ACACIA_LADDER, new Item.Settings().group(Vanillaful.VANILLAFUL)));
-		BlockRenderLayerMap.INSTANCE.putBlock(Vanillaful.ACACIA_LADDER, RenderLayer.getCutout());
 		//Dark Oak Stick
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "dark_oak_stick"), DARK_OAK_STICK);
 		//Dark Oak Ladder
 		Registry.register(Registry.BLOCK, new Identifier("vanillaful", "dark_oak_ladder"), DARK_OAK_LADDER);
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "dark_oak_ladder"), new BlockItem(DARK_OAK_LADDER, new Item.Settings().group(Vanillaful.VANILLAFUL)));
-		BlockRenderLayerMap.INSTANCE.putBlock(Vanillaful.DARK_OAK_LADDER, RenderLayer.getCutout());
 		//Crimson Stick
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "crimson_stick"), CRIMSON_STICK);
 		//Crimson Ladder
 		Registry.register(Registry.BLOCK, new Identifier("vanillaful", "crimson_ladder"), CRIMSON_LADDER);
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "crimson_ladder"), new BlockItem(CRIMSON_LADDER, new Item.Settings().group(Vanillaful.VANILLAFUL)));
-		BlockRenderLayerMap.INSTANCE.putBlock(Vanillaful.CRIMSON_LADDER, RenderLayer.getCutout());
 		//Warped Stick
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "warped_stick"), WARPED_STICK);
 		//Warped Ladder
 		Registry.register(Registry.BLOCK, new Identifier("vanillaful", "warped_ladder"), WARPED_LADDER);
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "warped_ladder"), new BlockItem(WARPED_LADDER, new Item.Settings().group(Vanillaful.VANILLAFUL)));
-		BlockRenderLayerMap.INSTANCE.putBlock(Vanillaful.WARPED_LADDER, RenderLayer.getCutout());
 
 		//Nether Brick Fence Gate
 		Registry.register(Registry.BLOCK, new Identifier("vanillaful", "nether_brick_fence_gate"), NETHER_BRICK_FENCE_GATE);
 		Registry.register(Registry.ITEM, new Identifier("vanillaful", "nether_brick_fence_gate"), new BlockItem(NETHER_BRICK_FENCE_GATE, new Item.Settings().group(Vanillaful.VANILLAFUL)));
-		//BlockRenderLayerMap.INSTANCE.putBlock(Vanillaful.NETHER_BRICK_FENCE_GATE, RenderLayer.getCutout());
 
+		//Render Ladders
+		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), Vanillaful.IRON_LADDER, Vanillaful.SPRUCE_LADDER, Vanillaful.BIRCH_LADDER, Vanillaful.JUNGLE_LADDER, Vanillaful.ACACIA_LADDER, Vanillaful.DARK_OAK_LADDER, Vanillaful.CRIMSON_LADDER, Vanillaful.WARPED_LADDER);
+
+		//Register Fuel
+		FuelRegistry.INSTANCE.add(Vanillaful.LADDERS_FUEL, 300);
+		FuelRegistry.INSTANCE.add(Vanillaful.STICKS_FUEL, 100);
 	}
 }
