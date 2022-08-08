49c2e97b0d20e299a37d17f1a26ddc39787b335a
Dan <baghdadi.danny@gmail.com>
2022-08-08 00:38:10 -0700

    Gel Fluid implementation

--- src/main/java/dev/danmizu/vanillaful/block/ModBlocks.java
+++ src/main/java/dev/danmizu/vanillaful/block/ModBlocks.java
@@ -2,9 +2,15 @@ package dev.danmizu.vanillaful.block;
 
 // Minecraft Imports
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FluidBlock;
 import net.minecraft.block.Material;
+import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.fluid.Fluid;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.BucketItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -17,28 +23,62 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import dev.danmizu.vanillaful.util.ModGroups;
 import dev.danmizu.vanillaful.util.ModInfo;
 import dev.danmizu.vanillaful.block.custom.FreeStandingLadderBlock;
+import dev.danmizu.vanillaful.block.custom.GelFluidBlock;
 
 public class ModBlocks {
 
     // Iron Ladder
     public static final Block IRON_LADDER = registerBlock("iron_ladder",
-            new FreeStandingLadderBlock(FabricBlockSettings.of(Material.METAL).sounds(BlockSoundGroup.METAL).strength(5, 6).nonOpaque()));
+            new FreeStandingLadderBlock(
+                    FabricBlockSettings.of(Material.METAL).sounds(BlockSoundGroup.METAL).strength(5, 6).nonOpaque()));
 
-    // Register Blocks without Items
-    // private static Block registerBlockWithoutBlockItem(String name, Block block){
-    //     return Registry.register(Registry.BLOCK, new Identifier(ModInfo.MOD_ID, name), block);
-    // }
+    // Gel
+    public static Item GEL_BUCKET;
+    public static final Fluid STILL_GEL = registerFluid("gel",
+            new GelFluidBlock.Still(), "gel_bucket", GEL_BUCKET);
+
+    // Gel (Flowing)
+    public static final Fluid FLOWING_GEL = registerFluidWithoutFluidItem("flowing_gel",
+            new GelFluidBlock.Flowing());
+
+    // Gel (Fluid Block)
+    public static final Block GEL = registerBlockWithoutBlockItem("gel",
+            new FluidBlock((FlowableFluid) STILL_GEL, FabricBlockSettings.copy(Blocks.WATER)));
 
     // Register Blocks
-    private static Block registerBlock(String name, Block block){
+    private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registry.BLOCK, new Identifier(ModInfo.MOD_ID, name), block);
+    }
+
+    // Register Blocks without Items
+    private static Block registerBlockWithoutBlockItem(String name, Block block) {
+        return Registry.register(Registry.BLOCK, new Identifier(ModInfo.MOD_ID,
+                name), block);
     }
 
     // Register Block Items
     private static Item registerBlockItem(String name, Block block) {
         return Registry.register(Registry.ITEM, new Identifier(ModInfo.MOD_ID, name),
                 new BlockItem(block, new FabricItemSettings().group(ModGroups.VANILLAFUL)));
+    }
+
+    // Register Fluid
+    private static Fluid registerFluid(String name, Fluid fluid, String fluidItemName, Item item) {
+        item = registerFluidItem(fluidItemName, fluid);
+        return Registry.register(Registry.FLUID, new Identifier(ModInfo.MOD_ID, name), fluid);
+    }
+
+    // Register Fluid Without Fluid Item
+    private static Fluid registerFluidWithoutFluidItem(String name, Fluid fluid) {
+        return Registry.register(Registry.FLUID, new Identifier(ModInfo.MOD_ID, name), fluid);
+    }
+
+    // Register Fluid Items (Buckets)
+    private static Item registerFluidItem(String name, Fluid fluid) {
+        return Registry.register(Registry.ITEM, new Identifier(ModInfo.MOD_ID, name),
+                new BucketItem(fluid, new FabricItemSettings().group(ModGroups.VANILLAFUL).recipeRemainder(Items.BUCKET)
+                        .maxCount(1)));
     }
 
     public static void registerModBlocks() {
--- src/main/java/dev/danmizu/vanillaful/block/custom/GelFluidBlock.java
+++ src/main/java/dev/danmizu/vanillaful/block/custom/GelFluidBlock.java
@@ -7,11 +7,17 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
+import net.minecraft.item.Item;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
+
+// Local Imports
+import dev.danmizu.vanillaful.block.ModBlocks;
 
 public abstract class GelFluidBlock extends FlowableFluid {
 	/**
@@ -21,7 +27,7 @@ public abstract class GelFluidBlock extends FlowableFluid {
 	public boolean matchesType(Fluid fluid) {
 		return fluid == getStill() || fluid == getFlowing();
 	}
- 
+
 	/**
 	 * @return whether the fluid infinite like water
 	 */
@@ -29,7 +35,7 @@ public abstract class GelFluidBlock extends FlowableFluid {
 	protected boolean isInfinite() {
 		return false;
 	}
- 
+
 	/**
 	 * Perform actions when fluid flows into a replaceable block. Water drops
 	 * the block's loot table. Lava plays the "block.lava.extinguish" sound.
@@ -39,7 +45,7 @@ public abstract class GelFluidBlock extends FlowableFluid {
 		final BlockEntity blockEntity = state.hasBlockEntity() ? world.getBlockEntity(pos) : null;
 		Block.dropStacks(state, world, pos, blockEntity);
 	}
- 
+
 	/**
 	 * Lava returns true if its FluidState is above a certain height and the
 	 * Fluid is Water.
@@ -47,10 +53,11 @@ public abstract class GelFluidBlock extends FlowableFluid {
 	 * @return whether the given Fluid can flow into this FluidState
 	 */
 	@Override
-	protected boolean canBeReplacedWith(FluidState fluidState, BlockView blockView, BlockPos blockPos, Fluid fluid, Direction direction) {
+	protected boolean canBeReplacedWith(FluidState fluidState, BlockView blockView, BlockPos blockPos, Fluid fluid,
+			Direction direction) {
 		return false;
 	}
- 
+
 	/**
 	 * Possibly related to the distance checks for flowing into nearby holes?
 	 * Water returns 4. Lava returns 2 in the Overworld and 4 in the Nether.
@@ -59,7 +66,7 @@ public abstract class GelFluidBlock extends FlowableFluid {
 	protected int getFlowSpeed(WorldView worldView) {
 		return 4;
 	}
- 
+
 	/**
 	 * Water returns 1. Lava returns 2 in the Overworld and 1 in the Nether.
 	 */
@@ -67,12 +74,70 @@ public abstract class GelFluidBlock extends FlowableFluid {
 	protected int getLevelDecreasePerBlock(WorldView worldView) {
 		return 1;
 	}
- 
+
 	/**
 	 * Water returns 5. Lava returns 30 in the Overworld and 10 in the Nether.
 	 */
 	@Override
 	public int getTickRate(WorldView worldView) {
 		return 5;
+	}
+
+	/**
+	 * Water and Lava both return 100.0F.
+	 */
+	@Override
+	protected float getBlastResistance() {
+		return 100.0F;
+	}
+
+	@Override
+	public Fluid getStill() {
+		return ModBlocks.STILL_GEL;
+	}
+
+	@Override
+	public Fluid getFlowing() {
+		return ModBlocks.FLOWING_GEL;
+	}
+
+	@Override
+	public Item getBucketItem() {
+		return ModBlocks.GEL_BUCKET;
+	}
+
+	@Override
+	protected BlockState toBlockState(FluidState fluidState) {
+		return ModBlocks.GEL.getDefaultState().with(Properties.LEVEL_15, getBlockStateLevel(fluidState));
+	}
+
+	public static class Flowing extends GelFluidBlock {
+		@Override
+		protected void appendProperties(StateManager.Builder<Fluid, FluidState> builder) {
+			super.appendProperties(builder);
+			builder.add(LEVEL);
+		}
+
+		@Override
+		public int getLevel(FluidState fluidState) {
+			return fluidState.get(LEVEL);
+		}
+
+		@Override
+		public boolean isStill(FluidState fluidState) {
+			return false;
+		}
+	}
+
+	public static class Still extends GelFluidBlock {
+		@Override
+		public int getLevel(FluidState fluidState) {
+			return 8;
+		}
+
+		@Override
+		public boolean isStill(FluidState fluidState) {
+			return true;
+		}
 	}
 }
