d29895ab81292e643e52050513d9c10cf12a3b72
Dan <baghdadi.danny@gmail.com>
2023-01-14 13:40:30 -0800

    Translucent/Cutout support on Fabric

--- fabric/src/main/java/dev/danmizu/vanillaful/fabric/VanillafulFabricClient.java
+++ fabric/src/main/java/dev/danmizu/vanillaful/fabric/VanillafulFabricClient.java
@@ -1,13 +1,27 @@
 package dev.danmizu.vanillaful.fabric;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+
+import net.minecraft.client.renderer.RenderType;
+
+import dev.danmizu.vanillaful.registry.ModBlocks;
 
 public class VanillafulFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         dev.danmizu.vanillaful.client.VanillafulClient.init();
-        // BlockRenderLayerMap.INSTANCE.putBlocks(RenderType.cutout(),
-        // AFBlocks.OAK_TABLE.get(),
-        // );
+
+        // Translucent Blocks
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderType.translucent(), ModBlocks.GEL_BLOCK.get(),
+                ModBlocks.WHITE_GEL_BLOCK.get(), ModBlocks.ORANGE_GEL_BLOCK.get(), ModBlocks.MAGENTA_GEL_BLOCK.get(),
+                ModBlocks.LIGHT_BLUE_GEL_BLOCK.get(), ModBlocks.YELLOW_GEL_BLOCK.get(), ModBlocks.LIME_GEL_BLOCK.get(),
+                ModBlocks.PINK_GEL_BLOCK.get(), ModBlocks.GRAY_GEL_BLOCK.get(), ModBlocks.LIGHT_GRAY_GEL_BLOCK.get(),
+                ModBlocks.CYAN_GEL_BLOCK.get(), ModBlocks.PURPLE_GEL_BLOCK.get(), ModBlocks.BLUE_GEL_BLOCK.get(),
+                ModBlocks.BROWN_GEL_BLOCK.get(), ModBlocks.GREEN_GEL_BLOCK.get(), ModBlocks.RED_GEL_BLOCK.get(),
+                ModBlocks.BLACK_GEL_BLOCK.get());
+
+        // Cutout Blocks
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderType.cutout(), ModBlocks.IRON_LADDER.get());
     }
 }
