44fd335e7a7e210924fdf45362efea2d516c1d51
Dan <baghdadi.danny@gmail.com>
2023-01-14 21:48:21 -0800

    iron ladder free standing functionality

--- common/src/main/java/dev/danmizu/vanillaful/block/IronLadderBlock.java
+++ common/src/main/java/dev/danmizu/vanillaful/block/IronLadderBlock.java
@@ -1,11 +1,12 @@
 package dev.danmizu.vanillaful.block;
 
-// import net.minecraft.core.BlockPos;
-// import net.minecraft.core.Direction;
-// import net.minecraft.world.level.BlockGetter;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.LadderBlock;
 import net.minecraft.world.level.block.SoundType;
-// import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 
 public class IronLadderBlock extends LadderBlock {
@@ -13,8 +14,12 @@ public class IronLadderBlock extends LadderBlock {
         super(Properties.of(Material.METAL).sound(SoundType.METAL).strength(0.4f, 6).noOcclusion());
     }
 
-    // Allow placement in non-traditional ladder ways
-    // private boolean canAttachTo(BlockGetter world, BlockPos pos, Direction side) {
-    //     return true;
-    // }
+    public boolean canAttachTo(BlockGetter blockReader, BlockPos pos, Direction direction) {
+        return true;
+    }
+
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return true;
+    }
 }
