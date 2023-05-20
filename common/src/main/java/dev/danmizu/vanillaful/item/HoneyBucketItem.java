6eae92e646e3311e6ac18777c3753b62d860867d
Dan <danmizu@nostalgi.cc>
2023-05-20 01:11:14 -0700

    dont consume in creative mode

--- common/src/main/java/dev/danmizu/vanillaful/item/HoneyBucketItem.java
+++ common/src/main/java/dev/danmizu/vanillaful/item/HoneyBucketItem.java
@@ -1,8 +1,10 @@
 package dev.danmizu.vanillaful.item;
 
 import dev.danmizu.vanillaful.registry.ModFoods;
+import dev.danmizu.vanillaful.registry.ModItems;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.HoneyBottleItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -25,10 +27,14 @@ public class HoneyBucketItem extends HoneyBottleItem {
 		// Remove Poison effect
 		livingEntity.removeEffect(MobEffects.POISON);
 
-		// Provide Honey Bucket food effects to player
+		// Provide food effects
 		livingEntity.eat(level, stack);
 
-		// Give back bucket on consumption
-		return new ItemStack(Items.BUCKET);
+		// Don't consume if in creative mode
+		if (((Player) livingEntity).isCreative()) {
+			return new ItemStack(ModItems.HONEY_BUCKET.get());
+		} else {
+			return new ItemStack(Items.BUCKET);
+		}
 	}
 }
